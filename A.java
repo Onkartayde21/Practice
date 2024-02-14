bea24e85f7e08596d2d28d6aeae3d0e59381d4bb
Rakesh Kumar Sah <rakeshkumar.sah@vodafone.com>
2024-02-14 11:52:41 +0530

    first commit

---
+++ A.java
@@ -0,0 +1,7 @@
+Class A
+{
+   public static void main(string []ar)
+{
+   system.out.println("Good Morning")
+}
+}
---
+++ qn2.java
@@ -0,0 +1,12 @@
+public class Interest  
+ {  
+   public static void main(String args[])  
+    {  
+        
+    float p, r,  t,  si;
+              p = 3000;  r = 10; t =5;  
+               si  = (p*r*t)/100;   
+              System.out.println("Simple Interest is: " +si);  
+    }
+     
+ }  
